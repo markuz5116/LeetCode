ebe05fd38f32c9dfec6d8dc6171b710db36cfefd
Marcus Ong <markuz5116@gmail.com>
2021-12-09 17:54:42 +0800

    Add findMedianSortedArrays solution

--- Solution.py
+++ Solution.py
@@ -26,3 +26,34 @@ class Solution:
                 return maxLen
             
         return maxLen
+
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        len_1 = len(nums1)
+        len_2 = len(nums2)
+        if len_1 > len_2:
+            return self.findMedianSortedArrays(nums2, nums1)
+        
+        total = len_1 + len_2
+        low = 0
+        high = len_1
+        
+        while low <= high:
+            mid_1 = low + int((high - low)/2)
+            mid_2 = int(total/2) - mid_1
+            
+            l1 = nums1[mid_1 - 1] if mid_1 != 0 else -999999
+            r1 = nums1[mid_1] if mid_1 != len_1 else 999999
+            
+            l2 = nums2[mid_2 - 1] if mid_2 != 0 else -999999
+            r2 = nums2[mid_2] if mid_2 != len_2 else 999999
+            
+            if l1 <= r2 and l2 <= r1:
+                if total % 2 == 0:
+                    return (max(l1, l2) + min(r1, r2)) / 2
+                return min(r1, r2)
+            elif l1 > r2:
+                high = mid_1 - 1
+            else:
+                low = mid_1 + 1
+        
+        return -1
